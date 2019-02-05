6f0771e12ec2478e25427b14b0184b82cc7fad31
Fernando Rocha <rocha@slb.com>
2019-02-04 22:04:56 -0600

    Is One Change Away

--- main.go
+++ main.go
@@ -26,6 +26,62 @@ func calculateArray(arr []int, pos int) int {
 	return result
 }
 
+func boggle(board [][]string, word string) {
+	fmt.Print("")
+}
+
+func isOneChangeAway(word string, word2 string) bool {
+	r := len(word2) - len(word)
+
+	if r == 0 {
+		sum := 0
+		for i := range word2 {
+			if word2[i] != word[i] {
+				sum++
+			}
+		}
+
+		if sum == 1 {
+			return true
+		}
+	} else if r == -1 {
+		for i := range word {
+			if i == len(word2) {
+				word2 = word2 + string(word[i])
+				if word2 == word {
+					return true
+				}
+			} else {
+				if word2[i] != word[i] {
+					word2 = word2[:i] + string(word[i]) + word2[i:len(word2)]
+					if word2 == word {
+						return true
+					}
+				}
+			}
+		}
+	} else if r == 1 {
+		for i := range word2 {
+			if i == len(word) {
+				word2 = word2[:len(word2)-1]
+				if word2 == word {
+					return true
+				}
+			} else {
+				if word2[i] != word[i] {
+					word2 = word2[:i] + word2[i+1:len(word2)]
+					if word2 == word {
+						return true
+					}
+					return false
+				}
+			}
+		}
+	}
+
+	return false
+}
+
 func fibo(n int) {
 	x, y := 0, 0
 	f := make([]int, n)
@@ -58,7 +114,22 @@ func fibo(n int) {
 // Bootstrap code {
 // Code snippet in grey is not editable and is necessary
 func main() {
-	fibo(10)
+	fmt.Println(isOneChangeAway("abc", "bbc"))
+	fmt.Println(isOneChangeAway("abc", "aba"))
+	fmt.Println(isOneChangeAway("abc", "acc"))
+	fmt.Println(isOneChangeAway("abc", "cbc"))
+	fmt.Println(isOneChangeAway("abc", "bca"))
+	fmt.Println(isOneChangeAway("abc", "c"))
+	fmt.Println(isOneChangeAway("abc", "ab"))
+	fmt.Println(isOneChangeAway("abc", "ac"))
+	fmt.Println(isOneChangeAway("abc", "bc"))
+	fmt.Println(isOneChangeAway("abc", "abca"))
+	fmt.Println(isOneChangeAway("abc", "aabc"))
+	fmt.Println(isOneChangeAway("abc", "abbc"))
+	fmt.Println(isOneChangeAway("abc", "dabc"))
+	fmt.Println(isOneChangeAway("abc", "cbassss"))
+
+	//fibo(10)
 	// scanner := bufio.NewScanner(os.Stdin)
 	// scanner.Scan() // use `for scanner.Scan()` to keep reading
 	// line := scanner.Text()
